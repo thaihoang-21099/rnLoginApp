1add8016db4af059e30828ce48cd162a35e78c33
Hoang21099 <hoang21099@gmail.com>
2019-11-17 15:56:56 +0700

    add Image background login screen

--- src/assets/Image.js
+++ src/assets/Image.js
@@ -1,12 +1,15 @@
 const Images = {
     BG_SPLASH:require('./images/bg-splash.png'),
+    BG_SCREEN6:require('./images/bg-screen6.png'),
     LOGO_SPLASH:require('./images/logo-splash.png'),
     LOGO:require('./images/logo.png'),
     ICON_FB:require('./images/icon-fb.png'),
     ICON_GMAIL:require('./images/icon-gmail.png'),
     ICON_PHONE:require('./images/icon-phone.png'),
     ICON_KEY:require('./images/icon-key.png'),
-    TRIANGLE_DOWN: require('./images/triangle-down.png')
+    TRIANGLE_DOWN: require('./images/triangle-down.png'),
+    IMG_CMND_FRONT:require('./images/cmnd-front.png'),
+    IMG_CMND_BACK:require('./images/cmnd-back.png'),
 }
 
 export default Images

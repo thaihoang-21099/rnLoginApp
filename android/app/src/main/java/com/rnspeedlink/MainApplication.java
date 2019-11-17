fa0936dd3291cefb2f2bf0b0ec50604a45f04be7
Hoang21099 <hoang21099@gmail.com>
2019-11-17 15:56:14 +0700

    add library react-native-web-view

--- android/app/src/main/java/com/rnspeedlink/MainApplication.java
+++ android/app/src/main/java/com/rnspeedlink/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.webview.RNCWebViewPackage;
 import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;

849a48d7f10972f05c9d006a0dcb794ceb4146e2
Hoang21099 <hoang21099@gmail.com>
2019-11-19 14:57:37 +0700

    fix InputField

--- src/component/InputField/InputField.js
+++ src/component/InputField/InputField.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
-import { TextInput } from 'react-native';
+import { TextInput, TouchableOpacity } from 'react-native';
 import { View, Text, } from 'native-base';
+
+import BaseComponent from '../BaseComponent';
 
 import Icon from 'react-native-vector-icons/AntDesign';
 
+import styles from './styles';
 
-
-class InputField extends React.Component {
+class InputField extends BaseComponent {
     constructor(props) {
         super(props);
         this.state = {
@@ -27,28 +29,45 @@ class InputField extends React.Component {
     }
 
 
-    onChange = (text)=>{
-        const  {onChangeText,name} = this.props;
-        onChangeText(name,text);
+    onChange = (text) => {
+        const { onChangeText, name } = this.props;
+        if(onChangeText){
+            onChangeText(name, text);
+        }
     }
     render() {
-        const { text, placeholder, isRequired, onChangeText, name, isLogin } = this.props;
-
+        const { text, placeholder, isRequired, onChangeText, name, onSelect,locationString, handleFocus,id, handleRef } = this.props;
         return (
             <View>
                 {placeholder ?
-                    <View inputInfo>
-                        <Text inputRequired>{`${text}${isRequired ? '(*)' : ''}`}</Text>
-                        <TextInput style={{padding:0}} onChangeText={this.onChange}  placeholder={placeholder} placeholderTextColor="#767676"></TextInput>
+                    <View inputInfo style={{alignItems:'center'}}>
+                        <Text style={styles.label} inputRequired>{`${text}${isRequired ? '(*)' : ''}`}</Text>
+                        <TextInput 
+                            style={styles.textInput} 
+                            blurOnSubmit={true}
+                            onChangeText={this.onChange} 
+                            placeholder={placeholder} 
+                            placeholderTextColor="#767676" 
+                            returnKeyType="next"
+                            onSubmitEditing={()=>{
+                                if(!handleFocus) return
+                                if(id<5){handleFocus(id+1)}}
+                            }
+                            ref={input=>{
+                                if(handleRef) handleRef(id,input)
+                            }}
+                            />
                     </View>
                     :
-                    <View inputInfo style={{ alignItems: 'center' }}>
-                        <Text inputRequired>{`${text}${isRequired ? '(*)' : ''}`}</Text>
-                        <Text>{this.state.selected}</Text>
-                        <View  style={{flexDirection:'row'}}>
-                            <Text style={{marginRight:15}}>TP.Ho Chi Minh</Text>
-                            <Icon onPress={this.onPress} name={this.state.nameIcon} />
-                        </View>
+                    <View inputInfo style={{ alignItems: 'center',}}>
+                        <Text style={styles.label} inputRequired>{`${text}${isRequired ? '(*)' : ''}`}</Text>
+                        <TouchableOpacity style={[styles.textInput,{alignItems:'flex-end'}]} onPress={() => onSelect()}>
+                            <View style={{ flexDirection: 'row', }}>
+                                <Text style={{ marginRight: 15 }}>{locationString?locationString:this.getString('CHOOSE_CITY')}</Text>
+                                <Icon onPress={this.onPress} name={this.state.nameIcon} />
+                            </View>
+                        </TouchableOpacity>
+
                     </View>
                 }
             </View>
---
+++ src/component/InputField/styles.js
@@ -0,0 +1,16 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+    label:{
+        flex:0,
+        paddingBottom:0,
+    },
+    textInput:{
+        flex:1,
+        textAlign:'right',
+        padding:0,
+
+    }
+})
+
+export default styles;

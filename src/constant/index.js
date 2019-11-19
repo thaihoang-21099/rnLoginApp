7282ff70408ad1a13bb16f94e9be41ccbe8645a6
Hoang21099 <hoang21099@gmail.com>
2019-11-19 14:56:49 +0700

    add folder constant and data City-district-ward

---
+++ src/constant/index.js
@@ -0,0 +1,92 @@
+export const city =[
+    {
+        id:1,
+        name:'HCM',
+        district:[
+            {   
+                id:1,
+                name:'Quận 1',
+                ward:[
+                    {
+                        id:1,
+                        name:'Bến Nghé'
+                    },
+                    {
+                        id:2,
+                        name:'Bến Thành'
+                    },
+                    // {
+                    //     id:3,
+                    //     name:'Cô Giang'
+                    // },
+                    // {
+                    //     id:4,
+                    //     name:'Cầu Kho'
+                    // },
+                    // {
+                    //     id:5,
+                    //     name:'Cầu Ông Lãnh'
+                    // },
+                    // {
+                    //     id:6,
+                    //     name:'Đa Kao'
+                    // },
+                    // {
+                    //     id:7,
+                    //     name:'Nguyễn Thái Bình'
+                    // },
+                    // {
+                    //     id:8,
+                    //     name:'Nguyễn Cư Trinh'
+                    // },
+                    // {
+                    //     id:9,
+                    //     name:'Phạm Ngũ Lão'
+                    // },
+                    // {
+                    //     id:10,
+                    //     name:'Tân Định'
+                    // },
+                ]
+            },
+            {id:2,name:'Quận 2'},
+            {id:3,name:'Quận 3'},
+            {id:4,name:'Quận 4'},
+            {id:5,name:'Quận 5'},
+            {id:6,name:'Quận 6'},
+            {id:7,name:'Quận 7'},
+            {id:8,name:'Quận 8'},
+            {id:9,name:'Quận 9'},
+            {id:10,name:'Quận 10'},
+            {id:11,name:'Quận 11'},
+            {id:12,name:'Quận 12'},
+            {id:13,name:'Quận Bình Tân'},
+            {id:14,name:'Quận Bình Thạnh'},
+            {id:15,name:'Quận Gò Vấp'},
+            {id:16,name:'Quận Phú Nhuận'},
+            {id:17,name:'Quận Tân Bình'},
+            {id:18,name:'Quận Tân Phú'},
+            {id:19,name:'Quận Thủ Đức'},
+            {id:20,name:'Quận Bình Chánh'},
+            {id:21,name:'Quận Cần Giờ'},
+            {id:22,name:'Quận Củ Chi'},
+            {id:23,name:'Quận Hóc Môn'},
+            {id:24,name:'Quận Nhà Bè'},
+        ]
+    },
+    {
+        id:2,
+        name:'Da nang',
+        district:[
+            {id:1,name:'Quận Cẩm Lệ'},
+            {id:2,name:'Quận Hải Châu'},
+            {id:3,name:'Quận Liên Chiểu'},
+            {id:4,name:'Quận Ngũ Hành Sơn'},
+            {id:5,name:'Quận Sơn Trà'},
+            {id:6,name:'Quận Thanh Kê'},
+            {id:7,name:'Quận Hoà Vang'},
+            {id:8,name:'Quận Hoàng Sa Island'},
+        ]
+    }
+]
+    

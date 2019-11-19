7a8fda6dbd50726e64bf608b20731a94a62aa4f5
Hoang21099 <hoang21099@gmail.com>
2019-11-19 14:45:21 +0700

    update title for Register Screen 4

--- src/screens/RegisterScreen/Screen4/Screen4.js
+++ src/screens/RegisterScreen/Screen4/Screen4.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import {View,Text,Button} from 'native-base'
+import { View, Text, Button } from 'native-base'
 
 //Components
 import BankInfoComponent from '../../../component/BankInfoComponent';
@@ -8,31 +8,34 @@ import BaseComponent from '../../../component/BaseComponent';
 
 //Style
 import styles from './styles';
+import I18N from '../../../local';
 
+const PAY_CLEARING = I18N.t("PAY_CLEARING");
+const PAY_CASH = I18N.t("PAY_CASH");
 class Screen4 extends BaseComponent {
-    static navigationOptions={
-        title:'Thanh toan can tru'
-    }
+    isClearing = this.props.navigation.state.params.isClearing;
+
+
+    static navigationOptions = ({ navigation,screenProps }) => ({
+        title: navigation.state.params.isClearing?PAY_CLEARING:PAY_CASH ,
+    });
+
     onChangeScreen = () => {
         this.props.navigation.navigate('RegisterScreen6')
     }
     render() {
-        const isCash = false;
         return (
             <View style={styles.container}>
                 <View style={styles.containerInner}>
-                {
-                    isCash
-                    ?
-                        <BankInfoComponent />
-                    :
-                        <BankInfoComponent onChangeScreen={this.onChangeScreen} image/>
-                }
-            </View>
+                    {
+                        <BankInfoComponent onChangeScreen={this.onChangeScreen} isClearing={this.isClearing} />
+                    }
+                </View>
             </View>
         )
     }
 }
+
 
 
 export default Screen4;

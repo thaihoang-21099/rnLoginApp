8c6e6619352cd34f4b262b4d092b31c997e8759b
Hoang21099 <hoang21099@gmail.com>
2019-11-25 14:09:53 +0700

    add Main screen in app navigator

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,39 +1,43 @@
 import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import { View } from 'react-native';
+
+import AppHeader from '../component/layout/AppHeader';
+
 
 import SplashScreen from '../screens/SplashSceen';
 import LoginScreen from '../screens/LoginScreen';
-// import RegisterScreen1 from '../screens/RegisterScreen/Screen1';
-// import RegisterScreen2 from '../screens/RegisterScreen/Screen2';
-// import RegisterScreen3 from '../screens/RegisterScreen/Screen3';
-// import RegisterScreen4 from '../screens/RegisterScreen/Screen4';
-// import RegisterScreen6 from '../screens/RegisterScreen/Screen6';
+
 
 import RegisterNavigator from './RegisterNavigator';
 
-import Colors from '../native-base-theme/Colors';
+
+import BottomNavigator from './BottomNavigator';
+
 
 const AppNavigator = createStackNavigator({
     SplashScreen: {
         screen: SplashScreen,
-
-
     },
     LoginScreen: {
         screen: LoginScreen,
         navigationOptions:{
-            gesturesEnabled: false,
+            header:null
         }
-
     },
     RegisterScreen: {
         screen: RegisterNavigator,
     },
-
+    MainScreen:{
+        screen:BottomNavigator,
+        navigationOptions:{
+            gesturesEnabled: false,
+        }
+    }
 },{
     defaultNavigationOptions:{
-        header:null
+        header: null
     }
 })
 

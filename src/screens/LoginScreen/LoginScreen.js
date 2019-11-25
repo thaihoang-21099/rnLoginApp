d019d22f4d248ddc2cb37a3400444e71faeaed00
Hoang21099 <hoang21099@gmail.com>
2019-11-25 14:08:26 +0700

    change style code screen Login

--- src/screens/LoginScreen/LoginScreen.js
+++ src/screens/LoginScreen/LoginScreen.js
@@ -4,7 +4,7 @@ import BaseComponent from '../../component/BaseComponent';
 import AppHeader from '../../component/layout/AppHeader';
 import Colors from '../../native-base-theme/Colors';
 
-import { StatusBar, Image, TextInput, SafeAreaView, KeyboardAvoidingView, ScrollView } from 'react-native';
+import { StatusBar, Image, TextInput, SafeAreaView, TouchableOpacity, ScrollView } from 'react-native';
 
 
 import Images from '../../assets/';
@@ -16,48 +16,82 @@ import styles from './styles';
 
 
 class LoginScreen extends BaseComponent {
-    inputs={}
+    inputs = {}
+
+    static navigationOptions = ({ navigation }) => ({
+        //header: null
+        title:'Hello'
+    });
 
     _onLogin = () => {
-        this.props.navigation.navigate('RegisterScreen1');
+        this.props.navigation.navigate('MainScreen');
+    }
+
+    _onRegister = () => {
+        this.props.navigation.navigate('RegisterScreen');
     }
 
     _focusTheField = (id) => {
         this.inputs[id].focus();
-      }
+    }
 
+    _renderSocialButton = (img, text, styleA) => {
+        return (
+            <Button style={styleA} socialLogin>
+                <Image resizeMode="contain" style={styles.iconSocial} source={img} />
+                <Text uppercase={false}>{text}</Text>
+            </Button>
+        )
+    }
+
+    _renderInput = (img,placeholder,onSubmit) => {
+        return (
+            <View input >
+                <Image style={styles.iconInput} source={Images.ICON_PHONE} />
+                <TextInput
+                    blurOnSubmit={false}
+                    returnKeyType={'next'}
+                    onSubmitEditing={() => { onSubmit }}
+                    style={styles.textInput}
+                    placeholderTextColor={Colors.TEXT_GRAY_LIGHT}
+                    placeholder={this.getString('PLACEHOLDER_USERNAME')} />
+            </View>
+        )
+    }
     render() {
         return (
 
             <ScrollView style={{ flex: 1 }} alwaysBounceVertical={false} showsVerticalScrollIndicator={false}>
                 <View style={styles.container}>
-                <SafeAreaView style={{flex:0}}/>
-                <StatusBar backgroundColor={Colors.BG_RED} barStyle="light-content" />
+                    <SafeAreaView style={{ flex: 0 }} />
+                    <StatusBar backgroundColor={Colors.BG_RED} barStyle="light-content" />
                     <View background>
                         <View style={styles.backgroundTop} />
                         <Image style={styles.triangleDown} source={Images.TRIANGLE_DOWN} resizeMode="stretch" />
                     </View>
-                    
+
                     <View style={styles.containerInner}>
                         <View style={styles.blockTop}>
                             <Logo />
                             <View input >
                                 <Image style={styles.iconInput} source={Images.ICON_PHONE} />
-                                <TextInput 
+                                <TextInput
                                     blurOnSubmit={false}
                                     returnKeyType={'next'}
                                     onSubmitEditing={() => { this._focusTheField('input2'); }}
-                                    style={styles.textInput} 
-                                    placeholderTextColor={Colors.TEXT_GRAY_LIGHT} 
+                                    style={styles.textInput}
+                                    placeholderTextColor={Colors.TEXT_GRAY_LIGHT}
                                     placeholder={this.getString('PLACEHOLDER_USERNAME')} />
                             </View>
+                            
+                            
                             <View input >
                                 <Image style={styles.iconInput} source={Images.ICON_KEY} />
-                                <TextInput  
+                                <TextInput
                                     ref={input => { this.inputs['input2'] = input }}
                                     onSubmitEditing={this._onLogin}
-                                    style={styles.textInput} 
-                                    placeholderTextColor={Colors.TEXT_GRAY_LIGHT} 
+                                    style={styles.textInput}
+                                    placeholderTextColor={Colors.TEXT_GRAY_LIGHT}
                                     placeholder={this.getString('PLACEHOLDER_PASSWORD')} />
                             </View>
                             <View style={{ alignItems: 'flex-end', marginBottom: 20 }}>
@@ -66,16 +100,18 @@ class LoginScreen extends BaseComponent {
                             <Button ref="login" login onPress={this._onLogin}><Text>{this.getString('LOGIN')}</Text></Button>
                         </View>
                         <View style={styles.blockDown}>
-                            <Button style={{ backgroundColor: '#0028a5' }} socialLogin>
-                                <Image resizeMode="contain" style={styles.iconSocial} source={Images.ICON_FB} />
-                                <Text uppercase={false}>{this.getString('LOGIN_FB')}</Text>
-                            </Button>
+                            {this._renderSocialButton(Images.ICON_GMAIL, this.getString('LOGIN_FB'), { backgroundColor: "#0028a5" })}
+                            {this._renderSocialButton(Images.ICON_GMAIL, this.getString('LOGIN_GG'))}
 
-                            <Button socialLogin>
-                                <Image resizeMode="contain" style={styles.iconSocial} source={Images.ICON_GMAIL} />
-                                <Text uppercase={false}>{this.getString('LOGIN_GG')}</Text>
-                            </Button>
-                            <Text style={{ color: 'black', marginTop: 20, textAlign: 'center' }}>{this.getString('NO_ACCOUNT')} <Text style={{ color: 'red' }}>{this.getString('SIGNUP_NOW')}</Text></Text>
+                            <TouchableOpacity onPress={this._onRegister}>
+                                <Text style={{ color: 'black', marginTop: 20, textAlign: 'center' }}>
+                                    {this.getString('NO_ACCOUNT')}
+                                    <Text style={{ color: 'red' }}>
+                                        {this.getString('SIGNUP_NOW')}
+                                    </Text>
+                                </Text>
+                            </TouchableOpacity>
+
                         </View>
                     </View>
                 </View>
@@ -83,6 +119,5 @@ class LoginScreen extends BaseComponent {
         )
     }
 }
-
 export default LoginScreen;
 

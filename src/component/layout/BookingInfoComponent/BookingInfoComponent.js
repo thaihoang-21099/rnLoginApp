4397308986bdc63803d965d7ac3d7b3c0c032a58
Hoang21099 <hoang21099@gmail.com>
2019-11-28 11:55:38 +0700

    fix style booking info component

--- src/component/layout/BookingInfoComponent/BookingInfoComponent.js
+++ src/component/layout/BookingInfoComponent/BookingInfoComponent.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { TouchableOpacity } from 'react-native';
 
-import {View,Text} from 'native-base';
+import { View, Text } from 'native-base';
 
 import EntypoIcon from 'react-native-vector-icons/Entypo';
 import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
@@ -15,13 +15,13 @@ import { currencyFormat } from '../../../helper';
 
 class BookingInfoComponent extends React.Component {
 
-     
 
-    _renderInfoDetail = (nameIcon, description,isPhone) => {
+
+    _renderInfoDetail = (nameIcon, description, isPhone) => {
         return (
             <View style={styles.infoDetail}>
                 <MaterialIcon name={nameIcon} size={15} />
-                <Text style={[styles.text,isPhone?{color:'blue'}:null]}>{description}</Text>
+                <Text style={[styles.text, isPhone ? { color: 'blue' } : null]}>{description}</Text>
             </View>
         )
     }
@@ -30,7 +30,7 @@ class BookingInfoComponent extends React.Component {
         return (
             <View style={styles.feeDetail}>
                 <Text style={styles.text}>{label}</Text>
-                <Text style={{fontWeight:"500",fontSize:13,color:'#3f3f3f'}}>{currencyFormat(fee)}</Text>
+                <Text style={{ fontWeight: "500", fontSize: 13, color: '#3f3f3f' }}>{currencyFormat(fee)}</Text>
 
             </View>
         )
@@ -40,7 +40,7 @@ class BookingInfoComponent extends React.Component {
         return (
             <TouchableOpacity style={[styles.blockAction, styleA]}>
                 <EntypoIcon style={{ marginRight: 5 }} name={nameIcon} color='red' />
-                <Text style={{color:'red'}}>{label}</Text>
+                <Text style={{ color: 'red' }}>{label}</Text>
             </TouchableOpacity>
         )
     }
@@ -50,34 +50,35 @@ class BookingInfoComponent extends React.Component {
         const { id, nameCus, address, phone, total, incurredFee, deliveryFee, date } = this.props.booking;
         const styleA = { borderLeftWidth: 1, borderLeftColor: 'white' }; //style border left for action block
         return (
-            
-                <View style={{ backgroundColor: 'white',
-                shadowColor: '#000',
-                shadowOffset: { width: 0, height: 0 },
-                shadowOpacity: 0.3,
-                shadowRadius: 1,
-                elevation: 2,
-                margin:10 }}>
-                    <View style={styles.blockTitle}>
-                        <Text style={styles.textTitle}>{id}</Text>
+
+            <View style={styles.container}>
+                <View style={styles.blockTitle}>
+                    <Text style={styles.textTitle}>{id}</Text>
+                    <View style={{
+                        marginLeft: 5,
+                        paddingHorizontal: 5,
+                        borderRadius:3,
+                        backgroundColor: 'red',
+                    }}>
                         <Text style={styles.textNew}>New</Text>
                     </View>
-                    <View style={styles.blockContent}>
-                        {this._renderInfoDetail('person', nameCus)}
-                        {this._renderInfoDetail('place', address)}
-                        {this._renderInfoDetail('phone-iphone', phone,true)}
-                    </View>
-                    <View style={styles.blockContent}>
-                        {this._renderFeeDetail('Tiền thu hộ', total)}
-                        {this._renderFeeDetail('Phí thu hộ', incurredFee)}
-                        {this._renderFeeDetail('Phí vận chuyển', deliveryFee)}
-                    </View>
-                    <View style={{ flexDirection: 'row', backgroundColor: '#ffe8e8', }}>
-                        {this._renderBlockAction('unread', 'Chi tiết')}
-                        {this._renderBlockAction('chat', 'Chat', styleA)}
-                        {this._renderBlockAction('dots-three-horizontal', null, styleA)}
-                    </View>
                 </View>
+                <View style={styles.blockContent}>
+                    {this._renderInfoDetail('person', nameCus)}
+                    {this._renderInfoDetail('place', address)}
+                    {this._renderInfoDetail('phone-iphone', phone, true)}
+                </View>
+                <View style={styles.blockContent}>
+                    {this._renderFeeDetail('Tiền thu hộ', total)}
+                    {this._renderFeeDetail('Phí thu hộ', incurredFee)}
+                    {this._renderFeeDetail('Phí vận chuyển', deliveryFee)}
+                </View>
+                <View style={{ flexDirection: 'row', backgroundColor: '#ffe8e8', }}>
+                    {this._renderBlockAction('unread', 'Chi tiết')}
+                    {this._renderBlockAction('chat', 'Chat', styleA)}
+                    {this._renderBlockAction('dots-three-horizontal', null, styleA)}
+                </View>
+            </View>
 
         )
     }
--- src/component/layout/BookingInfoComponent/styles.js
+++ src/component/layout/BookingInfoComponent/styles.js
@@ -1,4 +1,4 @@
-import {StyleSheet} from 'react-native';
+import { StyleSheet } from 'react-native';
 import Color from '../../../native-base-theme/Colors';
 import Measurement from '../../../native-base-theme/Measurements';
 import Font from '../../../native-base-theme/Fonts';
@@ -6,13 +6,18 @@ import Font from '../../../native-base-theme/Fonts';
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        backgroundColor: '#f1f1f1',
-        marginBottom:10,
-        
+        backgroundColor: 'white',
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 0 },
+        shadowOpacity: 0.3,
+        shadowRadius: 1,
+        elevation: 2,
+        marginTop: 10,
+        marginHorizontal: 10
+
     },
     blockAction: {
-        flexDirection:'row',
+        flexDirection: 'row',
         justifyContent: 'center',
         flex: 1,
         alignItems: 'center',
@@ -20,44 +25,42 @@ const styles = StyleSheet.create({
     },
     blockTitle: {
         paddingHorizontal: Measurement.mediumPadding,
-        paddingVertical:Measurement.basePadding,
+        paddingVertical: Measurement.basePadding,
         flexDirection: 'row',
         alignItems: 'center',
         borderBottomWidth: Measurement.border.small,
         borderBottomColor: '#f1f1f1'
     },
-    textTitle:{
+    textTitle: {
         color: '#3a5b9b',
-        fontWeight:'700' 
+        fontWeight: '700'
     },
-    textNew:{
-         marginLeft: Measurement.smallMargin, 
-         paddingHorizontal: Measurement.smallPadding, 
-         backgroundColor: Color.BG_RED, 
-         color: Color.TEXT_WHITE,
-         fontWeight:'500' ,
+    textNew: {
+        color: Color.TEXT_WHITE,
+        fontWeight: '500',
+        fontSize: 12,
     },
     blockContent: {
         paddingHorizontal: Measurement.mediumPadding,
-        paddingBottom:Measurement.basePadding,
+        paddingBottom: Measurement.basePadding,
         borderBottomWidth: Measurement.border.small,
         borderBottomColor: '#f1f1f1',
-        paddingVertical:5
+        paddingVertical: 5
     },
     feeDetail: {
         flexDirection: 'row',
         justifyContent: 'space-between',
-        paddingTop:Measurement.smallPadding
+        paddingTop: Measurement.smallPadding
     },
     infoDetail: {
         flexDirection: 'row',
         paddingTop: Measurement.smallPadding,
-        paddingRight:Measurement.basePadding
+        paddingRight: Measurement.basePadding
     },
-    text:{
-        fontSize:Font.Size.small,
-        marginLeft:Measurement.smallMargin,
-        color:'#3f3f3f'
+    text: {
+        fontSize: Font.Size.small,
+        marginLeft: Measurement.smallMargin,
+        color: '#3f3f3f'
     }
 
 })

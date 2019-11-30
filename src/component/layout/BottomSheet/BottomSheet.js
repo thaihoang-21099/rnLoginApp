3e16aefc18cc74f72c4cc622d4dad9d4c9e0b4ce
Hoang21099 <hoang21099@gmail.com>
2019-11-30 11:35:09 +0700

    add BottomSheet component

---
+++ src/component/layout/BottomSheet/BottomSheet.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import BottomSheet from 'react-native-bottomsheet';
+
+import { View } from 'react-native';
+
+import PropTypes from 'prop-types';
+
+import createReactClass from 'create-react-class';
+
+
+const DefaultTabBar = createReactClass({
+    propTypes: {
+        onGetFromLib: PropTypes.func,
+        onTakeAShot: PropTypes.func,
+    },
+
+    getDefaultProps() {
+        return {
+            onGetFromLib: () => { },
+            onTakeAShot: () => { }
+        };
+    },
+
+    render() {
+        const {onGetFromLib,onTakeAShot} = this.props
+        return (
+            <View>
+                {BottomSheet.showBottomSheetWithOptions({
+                    options: ['Chọn ảnh từ thư viên', 'Chụp ảnh', 'Huỷ'],
+                    dark: true,
+                    cancelButtonIndex: 2,
+                }, (value) => {
+                    switch (value) {
+                        case 0: { onGetFromLib(); return }
+                        case 1: { onTakeAShot(); return };
+                    }
+                })}
+            </View>
+        )
+    }
+})
+
+
+
+
+export default DefaultTabBar;
+
+
+
+
+
+
+
+// class BottomSheetComponent extends React.Component {
+//     render() {
+//         const {onGetFromLib,onTakeShot} = this.props;
+//         return(
+//         <View>
+//             {BottomSheet.showBottomSheetWithOptions({
+//                 options: ['Chọn ảnh từ thư viên', 'Chụp ảnh', 'Huỷ'],
+//                 dark: true,
+//                 cancelButtonIndex: 2,
+//             }, (value) => {
+//                 switch(value){
+//                     case 0: {onGetFromLib();return}
+//                     case 1: {onTakeShot();return};
+//                 }
+//             })}
+//         </View>
+//         )
+
+//     }
+// }
+
+//export default BottomSheetComponent;
